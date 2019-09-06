fba6addd26d9d4b4105c6ebcd90fcd1ff3ed9831
vivianmiwa <vivianfugihara@gmail.com>
2019-09-06 10:59:43 -0300

    terceiro commit

--- src/pages/beacon.js
+++ src/pages/beacon.js
@@ -1,15 +1,24 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, Button, Linking} from 'react-native';
+import { WebView } from 'react-native-webview';
 
 const Beacon = ({ navigation }) => (
   <View style = {styles.container}>
     <Text style = {styles.titulo}>{navigation.state.params.beacon.textos[0].titulo}</Text>
     <Text style = {styles.texto}>{navigation.state.params.beacon.textos[0].texto}</Text>
+    <Button title="Saiba mais" onPress={ ()=>{ Linking.openURL(navigation.state.params.beacon.links[0].url)}}/>
   </View>
 );
 
 Beacon.navigationOptions = ({ navigation }) => ({
-  title: navigation.state.params.beacon.nome
+  title: navigation.state.params.beacon.nome,
+  headerStyle: {
+  backgroundColor: '#008ae6',
+},
+  headerTintColor: '#fff',
+  headerTitleStyle: {
+    fontWeight: 'bold',
+  }
 });
 
 export default Beacon;
@@ -31,7 +40,7 @@ const styles = StyleSheet.create({
   },
 
   texto: {
-    flex: 8,
+    flex: 1,
     fontSize: 16,
     color: "#333",
     padding: 20,
